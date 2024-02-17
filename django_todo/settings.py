0f516cc2022e45e482aaf73de4d8d950b000122c
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-02-17 21:09:14 +0000

    Changed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://git.heroku.com/sarr-django-todo-app.git']
 
 
 # Application definition
